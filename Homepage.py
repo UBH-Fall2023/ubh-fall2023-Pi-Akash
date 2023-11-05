039aba1b829b76ff228d92886d6c2c0192f88762
Pi Akash <pi.akash96@gmail.com>
2023-11-04 23:05:21 -0400

    First Draft of the Homepage
    - Homepage built in Streamlit
    - Added Homepage.py First Draft
    - Added 1 test case for 150 Students, 8 Professors and 16 class sections.
    - Added a few files to gitignore.
    
    Co-Authored-By: Eugenia Vance <29989917+ginadotexe@users.noreply.github.com>

---
+++ Homepage.py
@@ -0,0 +1,147 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import os
+from itertools import combinations
+
+# page title 
+st.title("First Page")
+
+# file upload for Professor inputs
+professor_input = st.file_uploader(
+    label = "Please upload input file from Professors",
+    type = "csv"
+    )
+
+# reading professor inputs in a dataframe
+Professors_df = pd.read_csv(professor_input)
+
+# file upload for student inputs
+students_input = st.file_uploader(
+    label = "Please upload input file from Students",
+    type = "csv"
+)
+
+# reading student inputs in a dataframe
+Students_df = pd.read_csv(students_input)
+
+# Max intake of the class
+Max_Intake = max(Professors_df["Max Intake"])
+
+# Code generates class and students assigned to it based on Prority and their First selection
+
+# A new dictionary for storing key values pairs, key is the class and values will be students list
+Selection_1_dict = dict()
+
+# looping over all the unique classes present in Selection 1 of students
+for course in Students_df["Selection 1"].unique():
+    # condition if the courses are not already present in our selection 1 dictionary 
+    if course not in Selection_1_dict.keys():
+        # filter Students_df dataframe for all the students who has selected current course in selection 1
+        course_students = Students_df[Students_df["Selection 1"] == course][["Students", "Priority"]].reset_index(drop = True)
+        
+        # creating a dictionary of students name and their priority value 
+        course_students_dict = dict(zip(course_students["Students"], course_students["Priority"]))
+        
+        # sort the above dictionary based on the students priority value
+        course_students_dict = dict(sorted(course_students_dict.items(), key = lambda x: x[1]))
+        
+    # store the sorted students name list for each course in the dictionary
+    Selection_1_dict[course] = list(course_students_dict.keys())
+
+# sort the dictionary based on key values
+Selection_1_dict = dict(sorted(Selection_1_dict.items(), key = lambda x: x[0]))
+
+
+# we do the same thing for our Students 2
+# Code generates class and students assigned to it based on Prority and their First selection
+
+# A new dictionary for storing key values pairs, key is the class and values will be students list
+Selection_2_dict = dict()
+
+# looping over all the unique classes present in Selection 2 of students
+for course in Students_df["Selection 2"].unique():
+    # condition if the courses are not already present in our selection 2 dictionary 
+    if course not in Selection_2_dict.keys():
+        # filter Students_df dataframe for all the students who has selected current course in selection 2
+        course_students = Students_df[Students_df["Selection 2"] == course][["Students", "Priority"]].reset_index(drop = True)
+        
+        # creating a dictionary of students name and their priority value 
+        course_students_dict = dict(zip(course_students["Students"], course_students["Priority"]))
+        
+        # sort the above dictionary based on the students priority value
+        course_students_dict = dict(sorted(course_students_dict.items(), key = lambda x: x[1]))
+        
+    # store the sorted students name list for each course in the dictionary
+    Selection_2_dict[course] = list(course_students_dict.keys())
+
+# sort the dictionary based on key values
+Selection_2_dict = dict(sorted(Selection_2_dict.items(), key = lambda x: x[0]))
+
+# We create a final list of students for both selections 
+final_selection_list = dict()
+for i, j in zip(Selection_1_dict, Selection_2_dict):
+    final_selection_list[i] = Selection_1_dict[i] + Selection_2_dict[j]
+
+# create a list of all the classes available
+classes = list(final_selection_list.keys())
+
+# all the possible combinations in which we have to check for same student in multiple classes
+combinations_check = list(combinations(classes, 2))
+
+# check for all the combinations
+for combination in combinations_check:
+    final_selection_list[combination[1]] = [e for e in final_selection_list[combination[1]] if e not in final_selection_list[combination[0]][:Max_Intake]]
+
+
+# new dataframe for courses and their allotments
+courses = Professors_df["Course"]
+courses_students = np.zeros((len(courses), Max_Intake))
+courses_students = pd.DataFrame(courses_students, index = courses)
+
+# editing the previous table for allocations
+
+# looping over all the courses
+for course_index in courses_students.index:
+    # take the max intake size for each class
+    course_intake = Professors_df[Professors_df["Course"] == course_index]["Max Intake"].values[0]
+    for student_index in range(course_intake):
+        try:
+            courses_students.loc[course_index, student_index] = final_selection_list[course_index][student_index]
+        except Exception as e:
+            pass
+
+
+alloted_students = []
+# loop over all the positions in the dataframe
+for course_columns in courses_students.columns:
+    # loop over all the students assigned for different classes in that position
+    for student in courses_students[course_columns]:
+        # condition if students does not exist in alloted students list
+        if student not in alloted_students:
+            # append that student to the list
+            alloted_students.append(student)
+
+# list of all the students alloted to any of the class or sections 
+all_students_list = set(Students_df["Students"])
+
+# list of non alloted students
+non_alloted_students = all_students_list - set(alloted_students)
+
+# reset index of our course and allocated students dataframe
+course_students = courses_students.reset_index()
+n_rows, n_cols = course_students.shape
+
+# number of seats available in each class
+available_class_slots = pd.DataFrame(zip(course_students["Course"], np.sum(course_students == 0.0, axis = 1)))
+vacant_slots = np.sum(course_students == 0.0, axis = 1).sum()
+
+# Merge the professors dataframe with alloted students dataframe for final results
+course_students = Professors_df.merge(courses_students, how = "inner", left_on = "Course", right_on = "Course")
+
+
+# test code
+st.write(course_students)
+st.write("Total Slots Available for students : ", n_rows * n_cols)
+st.write("Total Slots vacant : ", vacant_slots)
+st.write(Students_df[Students_df["Students"].isin(non_alloted_students)])
